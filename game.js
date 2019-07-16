c26c6642e11296ac271a753885a52b1fb059c2ff
Yeon Lee <ginterlee29@gmail.com>
2019-07-16 01:11:43 -0400

    added some function to stop from clikcing before the screen turns into green

--- game.js
+++ game.js
@@ -1,11 +1,11 @@
 var screen = document.querySelector('#screen');
 
-var state = {
-    
-};
 var startTime;
 var endTime;
 
+var record = [];
+
+var timeOut;
  //performance.now() can be used, too for precision
 //console.time('time');
 screen.addEventListener('click', function(){
@@ -15,23 +15,32 @@ screen.addEventListener('click', function(){
     screen.classList.remove('waiting');
     screen.classList.add('ready');
     screen.textContent = 'when green, click';
-    setTimeout(function(){
+     timeOut = setTimeout(function(){
         startTime = new Date(); // new Date()를 하는 순간 그 시각이 저장된다. 
         screen.click();
     }, (Math.random() * 1000)+ 2000); // 0 ~ 1000 사이 수
 
-    } else if(screen.classList.contains('ready')){
-    screen.classList.remove('ready');
-    screen.classList.add('now');
-    screen.textContent = 'Click now!';
+    } else if(screen.classList.contains('ready')){ // ready state
+        if (!startTime) { // 부정 출발 undefined , 0, null, false, '', NaN
+            clearTimeout(timeOut );
 
+            screen.classList.remove('ready');
+            screen.classList.add('waiting');
+            screen.textContent = 'click and wait till the sreen turns into green';
+        } else{
+            screen.classList.remove('ready');
+            screen.classList.add('now');
+            screen.textContent = 'Click now!';
+        }
    } else if(screen.classList.contains('now')){
     
      endTime = new Date();
     console.log('반응속도',  (endTime-startTime)/1000); // 밀리초 단위 1 sec = 1000 millisec 
-     
+    record.push(endTime - startTime);
+    startTime = null;
+    endTime = null;
     screen.classList.remove('now');
     screen.classList.add('waiting');
-    screen.textContent = 'Click here to start!'
+    screen.textContent = 'Click here to start!';
    }
 })
